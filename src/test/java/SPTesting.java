1965f89f639397f007d500ce80282b5d177addd9
Mikail Sonmez <mikail4458@hotmail.com>
2022-09-08 17:29:38 +0300

    v1

---
+++ src/test/java/SPTesting.java
@@ -0,0 +1,38 @@
+
+
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+
+import java.sql.*;
+
+public class SPTesting {
+
+    Connection con=null;
+    Statement stmt=null;
+
+    ResultSet rs;
+
+    @BeforeClass
+    void setup() throws SQLException {
+        con= DriverManager.getConnection("jdbc:mysql://localhost:3306/classicmodels","root","dota483361483");
+    }
+
+    @AfterClass
+    void tearDown() throws SQLException {
+        con.close();
+    }
+
+    @Test(priority=1)
+    void test_storedProceduresExists() throws SQLException
+    {
+        stmt=con.createStatement();
+        rs=stmt.executeQuery("SHOW PROCEDURE STATUS WHERE Name='SelectAllCustomers'");
+        rs.next();
+
+        Assert.assertEquals(rs.getString("Name"),"SelectAllCustomers");
+    }
+}
