6a6ba687b59f54d0d92e3f1f98724608908ad4fa
Mikail Sonmez <mikail4458@hotmail.com>
2022-09-10 19:18:22 +0300

    v5

--- src/test/java/SPTesting.java
+++ src/test/java/SPTesting.java
@@ -52,6 +52,93 @@ public class SPTesting {
         Assert.assertEquals(compareResultSets(rs1, rs2), true);
     }
 
+    @Test(priority = 3)
+    void test_SelectAllCustomersByCity() throws SQLException
+    {
+        cStmt = con.prepareCall("{call SelectAllCustomersByCity(?)}");
+        cStmt.setString(1, "Singapore");
+        rs1 = cStmt.executeQuery(); // resultset1
+
+        Statement stmt = con.createStatement();
+        rs2 = stmt.executeQuery("SELECT * FROM Customers WHERE city = 'Singapore'");
+
+        Assert.assertEquals(compareResultSets(rs1,rs2),true);
+    }
+
+    @Test(priority = 4)
+    void test_SelectAllCustomersByCityAndPinCode() throws SQLException
+    {
+        cStmt = con.prepareCall("{call SelectAllCustomersByCityAndPin(?,?)}");
+        cStmt.setString(1, "Singapore");
+        cStmt.setString(2, "07993");
+        rs1 = cStmt.executeQuery(); // resultset1
+
+        Statement stmt = con.createStatement();
+        rs2 = stmt.executeQuery("SELECT * FROM Customers WHERE city = 'Singapore' and postalCode='07993'");
+
+        Assert.assertEquals(compareResultSets(rs1,rs2),true);
+    }
+
+    @Test(priority = 5)
+    void test_get_order_by_cust() throws SQLException {
+
+        cStmt = con.prepareCall("{call get_order_by_cust(?,?,?,?,?)}");
+        cStmt.setInt(1,141);
+
+        cStmt.registerOutParameter(2, Types.INTEGER);
+        cStmt.registerOutParameter(3, Types.INTEGER);
+        cStmt.registerOutParameter(4, Types.INTEGER);
+        cStmt.registerOutParameter(5, Types.INTEGER);
+
+        cStmt.executeQuery();
+
+        cStmt.getInt(2);
+
+        int shipped = cStmt.getInt(2);
+        int canceled = cStmt.getInt(3);
+        int resolved = cStmt.getInt(4);
+        int disputed = cStmt.getInt(5);
+
+        // System.out.println(shipped+" "+canceled+" "+resolved+" "+disputed);
+
+        Statement stmt=con.createStatement();
+        rs = stmt.executeQuery("select (SELECT count(*) as 'shipped' FROM orders WHERE customerNumber = 141 AND status = 'Shipped') as Shipped,(SELECT count(*) as 'canceled'FROM orders WHERE customerNumber = 141 AND status = 'Canceled') as Canceled,(SELECT count(*) as 'resolved'FROM orders WHERE customerNumber = 141 AND status = 'Resolved') as Resolved,(SELECT count(*) as 'disputed'FROM orders WHERE customerNumber = 141 AND status = 'Disputed') as Disputed");
+
+        rs.next();
+
+        int exp_shipped = rs.getInt("shipped");
+        int exp_canceled = rs.getInt("canceled");
+        int exp_resolved = rs.getInt("resolved");
+        int exp_disputed = rs.getInt("disputed");
+
+        if(shipped==exp_shipped && canceled==exp_canceled && resolved==exp_resolved && disputed== exp_disputed)
+            Assert.assertTrue(true);
+        else
+            Assert.assertTrue(false);
+    }
+
+    @Test(priority = 6)
+    void test_GetCustomerShipping() throws SQLException {
+
+        cStmt = con.prepareCall("{call GetCustomerShipping(?,?)}");
+        cStmt.setInt(1,112);
+        cStmt.registerOutParameter(2, Types.VARCHAR);
+
+        cStmt.executeQuery();
+
+        String shippedTime = cStmt.getString(2);
+
+        Statement stmt=con.createStatement();
+        rs = stmt.executeQuery("SELECT country,CASE WHEN country='USA' THEN '2-day Shipping' WHEN country='Canada' THEN '3-day Shipping' ELSE '5-day Shipping' END as ShippingTime FROM customers WHERE customerNumber=112");
+
+        rs.next();
+
+        String exp_shippingTime = rs.getString("ShippingTime");
+
+        Assert.assertEquals(shippedTime,exp_shippingTime);
+
+    }
+
     public boolean compareResultSets(ResultSet resultSet1, ResultSet resultSet2) throws SQLException {
         while (resultSet1.next()) {
 
