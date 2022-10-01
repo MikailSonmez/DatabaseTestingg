d3e3cf1fc88f92a780b5516fb4c3704da42b7c79
Mikail Sonmez <mikail4458@hotmail.com>
2022-10-01 23:58:23 +0300

    v11

--- src/test/java/SPTesting.java
+++ src/test/java/SPTesting.java
@@ -24,6 +24,7 @@ public class SPTesting {
     @BeforeClass
     void setup() throws SQLException {
         con = DriverManager.getConnection("jdbc:mysql://localhost:3306/classicmodels", "root", "********");
+
     }
 
     @AfterClass
